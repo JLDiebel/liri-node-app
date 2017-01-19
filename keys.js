a66fda2d1d2f20098584987115bcb23e7bd0eb2b
JLDiebel <joshua.l.diebel@gmail.com>
2017-01-19 11:49:38 -0600

    saving progress

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'gFkxZH8WMJ3WkSz6UM8H5cyis',
+  consumer_secret: 'EzDoRzLXMVgiZn9JJm07Esa9snB9K3OOHGoOt5M4A9KzBJi98W',
+  access_token_key: '822085294434426880-UgpLA8CEFLRZ9alSB57hWw5RxgbhMWA',
+  access_token_secret: 'p9bhWYGFPdCGySC4f1cfqbVZkzipMaJKpskJxthKKdZjW',
+}
